f51e2ca4b5122f7b4e90aabe34623373f1c589f3
jeetusingh247 <jeetusingh11010@gmail.com>
2025-11-19 18:14:16 +0530

    destructured

--- 01_hello_world/hello.js
+++ 01_hello_world/hello.js
@@ -1,3 +1,8 @@
+// const math = require("./math"); 
+const { add, sub, mul } = require("./math"); // destructured
 
+// console.log("Addition is : ", math.add(2, 5));
+// console.log("Subtraction is : ", math.sub(9, 10));
 
-console.log("Addition is : ", add(2, 5));
+console.log("Addition : ", add(2, 5));
+console.log("Subtraction : ", sub(14, 12));
