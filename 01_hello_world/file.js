2d2f1b2ad9d989c925ffe21da9608e955843861f
jeetusingh247 <jeetusingh11010@gmail.com>
2025-11-19 19:14:45 +0530

    file handling

--- 01_hello_world/file.js
+++ 01_hello_world/file.js
@@ -4,4 +4,21 @@ const fs = require("fs");
 // fs.writeFileSync('./test.txt', 'Hey There');
 
 // Asynchronous
-fs.writeFile("./test.txt", "Hello World", (err) => {});
+// fs.writeFile("./test.txt", "Hello World", (err) => {});
+
+
+// read data from a file in dir - Sync...
+// const result = fs.readFileSync("./contacts.txt", "utf-8");
+// console.log("Contact No : ", result);
+
+fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+    if (err) {
+        console.log("Error", err);
+    } else {
+        console.log(result);
+    }
+});
+
+fs.appendFileSync("./test.txt", `${Date.now()}  \nHey There`);
+
+fs.copyFileSync("./test.txt", "./copy.txt");
