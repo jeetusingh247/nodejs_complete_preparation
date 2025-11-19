90cce62269e57e1b3e62cd39ac530e871dd1bcb9
jeetusingh247 <jeetusingh11010@gmail.com>
2025-11-19 18:13:24 +0530

    added anonymous function for using specific properties

--- 01_hello_world/math.js
+++ 01_hello_world/math.js
@@ -1,3 +1,19 @@
-function add(a, b){
-    return a + b;
-}
+// function add(a, b){
+//     return a + b;
+// }
+
+// function sub(a, b){
+//     return a - b;
+// }
+
+// function mul(a, b){
+//     return a * b;
+// }
+
+// module.exports = {add, sub, mul};
+
+exports.add = (a, b) => a + b;
+
+exports.sub = (a, b) => a - b;
+
+exports.mul = (a,b) => a * b;
