91ed0db995b4230538cbaab462751bce9f43d924
jeetusingh247 <jeetusingh11010@gmail.com>
2025-11-22 10:31:44 +0530

    to generate refresh, access tokens

---
+++ Projects/02_Role_Based_Access_Control/backend/generate_secrets/generateSecrets.js
@@ -0,0 +1,4 @@
+import crypto from "crypto";
+
+console.log("ACCESS_TOKEN_SECRET = " + crypto.randomBytes(64).toString("hex"));
+console.log("REFRESH_TOKEN_SECRET = " + crypto.randomBytes(64).toString("hex"));
